c5d963f03acf28f70d0b5d8b6d22d2303c4d0fcf
Andrew Muratov <imnebbish@proton.me>
2025-02-06 21:45:17 -0500

    Add frame-based animation

--- main.py
+++ main.py
@@ -38,5 +38,23 @@ def render_pattern(stdscr, pattern):
         for x, char in enumerate(row):
             stdscr.addch(y, x, char)
 
+def draw_kaleidoscope(stdscr):
+    frame = 0
+    while True:
+        stdscr.clear()
+        height, width = stdscr.getmaxyx()
+        pattern = generate_pattern(height - 2, width - 2)
+
+        for y in range(height - 2):
+            for x in range(width - 2):
+                stdscr.addch(y + 1, x + 1, pattern[y][x])
+
+        stdscr.refresh()
+        frame += 1
+        time.sleep(0.1)
+
+        if stdscr.getch() == ord('q'):
+            break
+
 if __name__ == "__main__":
     curses.wrapper(main)
